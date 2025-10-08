236a3b1936d55358c294e67aa23ad5558361b8e1
Michael <michael.elliston95@gmail.com>
2025-10-08 12:43:06 -0500

    DateTimeFormatter added and correctly formats the date and time

--- src/main/java/FormatDates.java
+++ src/main/java/FormatDates.java
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
     public static void main(String[] args) {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm");
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String formattedDate = currentDateTime.format(fmt);
+        String timeZone = "GMT";
+
+        System.out.println("Today is: " + formattedDate + " " + timeZone);
 
     }
 }
